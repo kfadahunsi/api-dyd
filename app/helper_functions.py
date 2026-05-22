27c68e1692eb7e56524937a6f22afdcec5abd249
Kevwe Fadahunsi <25905286+kfadahunsi@users.noreply.github.com>
2026-05-22 14:02:32 +0100

    error hanling for json loader

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -14,7 +14,7 @@ def load_json(file_path):
     path = Path(file_path)
     
     if not path.exists():
-        return None
+        raise FileNotFoundError(f"File not found: {path.resolve()}")
     
     with open(path, "r", encoding="utf-8") as f:
         return json.load(f)
